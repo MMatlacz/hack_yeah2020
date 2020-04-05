d05958a104c9344a926300444073e72884305fbe
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2020-04-05 09:01:45 +0200

    add action to clear accepted_by and accepted_at

--- backend/apps/help_requests/admin.py
+++ backend/apps/help_requests/admin.py
@@ -1,3 +1,6 @@
+from flask import flash
+
+from flask_admin.actions import action
 from flask_admin.contrib.sqla import ModelView
 
 from apps.extensions import (
@@ -10,6 +13,35 @@ from . import models
 
 class HelpRequestModelView(ModelView):
     column_display_pk = True
+
+    @action(
+        'clear_accepted_by',
+        'Clear accepted_by',
+        (
+            'Are you sure you want to clear `accepted_by` field for '
+            + 'selected help requests?'
+        ),
+    )
+    def clear_accepted_by(self, ids):
+        try:
+            self.session.bulk_update_mappings(
+                self.model,
+                [
+                    {'id': user_id, 'accepted_by_id': None, 'accepted_at': None}
+                    for user_id in ids
+                ],
+            )
+            self.session.flush()
+            self.session.commit()
+            flash(
+                f'Successfully cleared accepted_by of {len(ids)} help requests',
+                'success',
+            )
+        except Exception as exc:
+            if not self.handle_view_exception(exc):
+                raise
+
+            flash(f'Failed to clear approved_by. {exc}', 'error')
 
 
 admin.add_view(HelpRequestModelView(models.HelpRequest, db.session))
